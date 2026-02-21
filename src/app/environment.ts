9c70b3148fc76c2ac464c7680b2436fd8df6c00b
[your <61927341+chsanjay920@users.noreply.github.com>
2026-02-21 18:00:28 +0530

    Reverted Env Configuration

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
-// export const environment = {
-//   apiUrl: 'https://janagam-community-api.vercel.app',
-// };
 export const environment = {
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://janagam-community-api.vercel.app',
 };
+// export const environment = {
+//   apiUrl: 'http://localhost:5000',
+// };
